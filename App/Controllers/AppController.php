5ec8ef1d7300ed28065b535ca4e3d77907958e2a
Jéssica Bernardi Petersen <jessica041ster@gmail.com>
2021-05-07 16:42:21 -0300

    paginacao

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -15,12 +15,17 @@ class AppController extends Action {
 			
 		//recuperação dos tweets
 		$tweet = Container::getModel('Tweet');
-
 		$tweet->__set('id_usuario', $_SESSION['id']);
-
-		$tweets = $tweet->getAll();
+		//usuarios de paginacao
+		$total_registros_pagina = 10;
+		$pagina= isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+		$deslocamento = ($pagina - 1) * $total_registros_pagina;
+		$tweets = $tweet->getPorPagina($total_registros_pagina, $deslocamento);
 
 		$this->view->tweets = $tweets;
+		$total_tweets = $tweet->getTotalRegistros();
+		$this->view->total_de_paginas = ceil($total_tweets[0]['total'] / $total_registros_pagina);
+		$this->view->pagina_ativa = $pagina;
 
 
 		$usuario = Container::getModel('Usuario');
